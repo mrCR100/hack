583d644edaf29d0bccc98baaae9c62dfad8ee712
chenr <604441296@qq.com>
2025-03-19 13:37:40 +0800

    add install yolo on orin playbook

---
+++ playbooks/orin_yolo_install/pkgs/install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -ue
+
+SCRIPT_PATH=$(readlink -f "$0")
+
+if docker --version > /dev/null 2>&1; then
+    echo "docker is already installed"
+    docker --version
+else
+    cd "$SCRIPT_PATH"/docker
+    dpkg -i ./*.deb
+fi
+
+if nvidia-container-cli --version > /dev/null 2>&1; then
+    echo "nvidia container runtime is already installed"
+    nvidia-container-cli --version
+else
+    cd "$SCRIPT_PATH"/nvidia-container-runtime
+    dpkg -i ./*.deb
+    nvidia-ctk runtime configure --runtime=docker
+    systemctl restart docker
+fi
+
+configured_nvidia_runtime=$(docker info|grep Runtimes|grep nvidia)
+
+echo "$configured_nvidia_runtime"
